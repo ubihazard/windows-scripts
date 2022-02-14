8061e31220122ec61891dfce45b47e9b78cbafa0
Ubihazard <ubihazard@users.noreply.github.com>
2022-02-14 14:01:17 +0300

    Formatting

--- cleanup.bat
+++ cleanup.bat
@@ -6,17 +6,8 @@ reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\WordWheelQue
 :: PowerShell
 del /q /f "%APPDATA%\Microsoft\Windows\PowerShell\PSReadline\*.txt"
 
-:: WSL
-del /q /f "%LOCALAPPDATA%\lxss\home\%USERNAME%\.bash_history"
-
 :: WinRAR
 reg delete "HKCU\Software\WinRAR\ArcHistory" /f
-
-:: MuPDF
-del /q /f "%USERPROFILE%\.mupdf.history"
-
-:: Node.js
-del /q /f "%USERPROFILE%\.node_repl_history"
 
 :: CMake
 reg delete "HKCU\Software\Kitware" /f
@@ -26,6 +17,11 @@ reg delete "HKCU\Software\Kitware" /f
 ::rmdir /s /q "%APPDATA%\NVIDIA\GLCache"
 
 :: Other
-rmdir /s /q "%USERPROFILE%\.thumbnails"
 rmdir /s /q "%LOCALAPPDATA%\fontconfig"
+del /q /f "%LOCALAPPDATA%\lxss\home\%USERNAME%\.bash_history"
+rmdir /s /q "%USERPROFILE%\.thumbnails"
 del /q /f "%USERPROFILE%\.recently-used.xbel"
+del /q /f "%USERPROFILE%\.bash_history"
+del /q /f "%USERPROFILE%\.lesshst"
+del /q /f "%USERPROFILE%\.mupdf.history"
+del /q /f "%USERPROFILE%\.node_repl_history"
