6ee60924645b420908b73715a1cd1aede305feac
Ubihazard <ubihazard@users.noreply.github.com>
2022-10-01 02:13:00 +0300

    Update

--- cleanup.bat
+++ cleanup.bat
@@ -12,16 +12,12 @@ reg delete "HKCU\Software\WinRAR\ArcHistory" /f
 :: CMake
 reg delete "HKCU\Software\Kitware" /f
 
-:: NVIDIA
-::rmdir /s /q "%APPDATA%\NVIDIA\ComputeCache"
-::rmdir /s /q "%APPDATA%\NVIDIA\GLCache"
-
 :: Other
 rmdir /s /q "%LOCALAPPDATA%\fontconfig"
-del /q /f "%LOCALAPPDATA%\lxss\home\%USERNAME%\.bash_history"
 rmdir /s /q "%USERPROFILE%\.thumbnails"
 del /q /f "%USERPROFILE%\.recently-used.xbel"
 del /q /f "%USERPROFILE%\.bash_history"
 del /q /f "%USERPROFILE%\.lesshst"
 del /q /f "%USERPROFILE%\.mupdf.history"
 del /q /f "%USERPROFILE%\.node_repl_history"
+del /q /f "%LOCALAPPDATA%\lxss\home\%USERNAME%\.bash_history"
