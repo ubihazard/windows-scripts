5f121738508feabe09caad35a477e669b6c24fa7
Ubihazard <ubihazard@users.noreply.github.com>
2022-11-23 04:26:41 +0300

    Update

--- disable_office_ad.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-:: Disable M$ Office advertisment when pressing Ctrl+Alt+Shift+Win.
-
-reg add "HKCU\Software\Classes\ms-officeapp\shell\open\command" /ve /t REG_SZ /d rundll32 /f
-pause
